695ca674ce4cd4d7565cbdd08986394d65fac90e
IlmarsXd <ilmars500@gmail.com>
2023-02-08 16:20:12 +0200

    added TextUtils

---
+++ src/main/kotlin/dulkirmod/utils/TextUtils.kt
@@ -0,0 +1,24 @@
+package dulkirmod.utils
+
+import dulkirmod.DulkirMod
+import net.minecraft.util.ChatComponentText
+
+object TextUtils {
+	fun info(text: String, prefix: Boolean = true) {
+		val textPrefix = if (prefix) "${DulkirMod.CHAT_PREFIX} " else ""
+		DulkirMod.mc.thePlayer.addChatMessage(ChatComponentText("$textPrefix$text§r"))
+	}
+
+	fun toggledMessage(message: String, state: Boolean) {
+		val stateText = if (state) "§aON" else "§cOFF"
+		info("§9Toggled $message §8[$stateText§8]§r")
+	}
+
+	fun sendPartyChatMessage(message: String) {
+		sendMessage("/pc $message")
+	}
+
+	fun sendMessage(message: String) {
+		DulkirMod.mc.thePlayer.sendChatMessage(message)
+	}
+}
