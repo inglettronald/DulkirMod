6e1312acd491ea92144eff1da3652b968fd5fdcc
inglettronald <inglettronald@gmail.com>
2023-05-19 05:39:11 -0500

    slight performance fix

--- src/main/kotlin/dulkirmod/features/KeeperWaypoints.kt
+++ src/main/kotlin/dulkirmod/features/KeeperWaypoints.kt
@@ -11,20 +11,20 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import kotlin.math.max
 
 object KeeperWaypoints {
+    val vec1 = Vec3(-208.5, 44.5, -259.5)
+    val vec2 = Vec3(-311.5, 43.5, -232.5)
+    val vec3 = Vec3(-230.5, 57.5, -307.5)
+    val vec4 = Vec3(-269.5, 47.5, -166.5)
+    val vec5 = Vec3(-292.5, 47.5, -167.5)
+    val vec6 = Vec3(-291.5, 47.5, -183.5)
+    val vec7 = Vec3(-282.5, 47.5, -195.5)
+    val vec8 = Vec3(-262.5, 49.5, -191.5)
+    val vec9 = Vec3(-269.5, 61.5, -159.5)
     @SubscribeEvent
     fun onWorldRenderLast(event: RenderWorldLastEvent) {
         if (!DulkirConfig.keeperWaypoints) return
         if (TabListUtils.area != "Spider's Den") return
 
-        val vec1 = Vec3(-208.5, 44.5, -259.5)
-        val vec2 = Vec3(-311.5, 43.5, -232.5)
-        val vec3 = Vec3(-230.5, 57.5, -307.5)
-        val vec4 = Vec3(-269.5, 47.5, -166.5)
-        val vec5 = Vec3(-292.5, 47.5, -167.5)
-        val vec6 = Vec3(-291.5, 47.5, -183.5)
-        val vec7 = Vec3(-282.5, 47.5, -195.5)
-        val vec8 = Vec3(-262.5, 49.5, -191.5)
-        val vec9 = Vec3(-269.5, 61.5, -159.5)
         val playerVec = mc.thePlayer.positionVector
 
         val scale1 = max(1f, playerVec.distanceTo(vec1).toFloat()/10f)
