4b938eeaafdda5c28071b1587aed5e4fac5802c1
inglettronald <inglettronald@gmail.com>
2023-05-25 17:55:50 -0500

    comments to clarify usage

--- src/main/java/dulkirmod/mixins/MixinEntity.java
+++ src/main/java/dulkirmod/mixins/MixinEntity.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+
+// For MixinEntityLivingBase to extend
 @Mixin(value = {Entity.class}, priority = 800)
 public abstract class MixinEntity {
     @Shadow
