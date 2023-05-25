cf95db1da4bbbf6a08d2ac45241bf5f24e939a39
inglettronald <inglettronald@gmail.com>
2023-05-25 18:04:12 -0500

    hopefully better sound override

--- src/main/kotlin/dulkirmod/features/dungeons/SecretSounds.kt
+++ src/main/kotlin/dulkirmod/features/dungeons/SecretSounds.kt
@@ -83,7 +83,7 @@ object SecretSounds {
 
     fun playSound() {
         if (System.currentTimeMillis() - lastSound > 50) { // don't kill ears
-            val prevNote = mc.gameSettings.getSoundLevel(SoundCategory.MASTER)
+            val prevNote = mc.gameSettings.getSoundLevel(SoundCategory.RECORDS)
             mc.gameSettings.setSoundLevel(SoundCategory.RECORDS, 1f)
             mc.thePlayer.playSound("note.pling", 1f * DulkirConfig.secretSoundVolume, 1f)
             lastSound = System.currentTimeMillis()
