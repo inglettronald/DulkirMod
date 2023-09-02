a73b2f58a1e8ca8501b167bcaa9689fbc9d11f24
LOSEARDES77 <84414230+LOSEARDES77@users.noreply.github.com>
2023-09-02 22:51:38 +0200

    Update JoinDungeonCommand.kt

--- src/main/kotlin/dulkirmod/command/JoinDungeonCommand.kt
+++ src/main/kotlin/dulkirmod/command/JoinDungeonCommand.kt
@@ -4,29 +4,73 @@ import dulkirmod.config.DulkirConfig
 import dulkirmod.utils.TextUtils
 import net.minecraft.command.CommandException
 import net.minecraft.command.ICommandSender
+import java.util.*
 
 class JoinDungeonCommand : ClientCommandBase("joindungeon") {
     @Throws(CommandException::class)
     override fun processCommand(sender: ICommandSender, args: Array<String>) {
-        val arguments = args.contentToString().replace("[", "").replace("]", "").replace(",", "")
-        var type = ""
-        var num = ""
-        if (args[0].lowercase() == "master_catacombs") {
-            type = "M"
-        } else if (args[0].lowercase() == "catacombs") {
-            type = "F"
-        }
-
+        val list : List<String> = args[0].split("")
+        var floorType : String = list[0]
+        var floorNumber : String = list[1]
+        var num : Int = 0
         // Try statement so message is consistent if user gives bad input
         try {
-            if (args[1].toInt() in 1..7) {
-                num = args[1]
+            if (floorNumber.toInt() in 1..7) {
+                num = floorNumber.toInt()
             }
         } catch (_: NumberFormatException) {}
 
         if (DulkirConfig.dungeonCommandConfirm) {
-            TextUtils.info("§6Running command: $type$num")
+            TextUtils.info("§6Running command: $floorType$floorNumber")
         }
-        TextUtils.sendMessage("/joindungeon $arguments")
+        TextUtils.sendMessage("/joindungeon ${parse(floorType)}_FLOOR_${parse(num)}")
+    }
+
+    private fun parse(num: Int): String {
+        return when (num) {
+            1 -> {
+                "ONE"
+            }
+            2 -> {
+                "TWO"
+            }
+            3 -> {
+                "THREE"
+            }
+            4 -> {
+                "FOUR"
+            }
+            5 -> {
+                "FIVE"
+            }
+            6 -> {
+                "SIX"
+            }
+            7 -> {
+                "SEVEN"
+            }
+            else -> {
+                TextUtils.info("§cCan't identify floor number.")
+                TextUtils.info("§cUsage: /joindungeon f5|m3|f7|m4 ")
+                ""
+            }
+        }
+    }
+
+
+    private fun parse(floor: String): String {
+        return when (floor.lowercase()){
+            "f" -> {
+                "CATACOMBS"
+            }
+            "m" -> {
+                "MASTER_CATACOMBS"
+            }
+            else -> {
+                TextUtils.info("§cCan't identify dungeon type.")
+                TextUtils.info("§cUsage: /joindungeon f5|m3|f7|m4 ")
+                ""
+            }
+        }
     }
 }
